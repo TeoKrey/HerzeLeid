611e00381750c780022b87f81c39e823ac01daf1
TeoKrey <klipy.bombs@gmail.com>
2026-09-08 15:11:23 +0300

    Move database URL to environment variables

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,23 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql+psycopg://postgres:1111@localhost:5432/herzelied"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL, future=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    future=True,
+)
+
 Base = declarative_base()
-    
